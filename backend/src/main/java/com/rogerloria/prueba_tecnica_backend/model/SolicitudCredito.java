7d4e8d39f9750a918b90dcf04b57a97fb86c82b1
rogerloria <rgrloria@gmail.com>
2025-04-01 11:27:42 -0600

    Ajuste: Ajustar el pom par aeliminar la dependencia de lombok y agregar los contructores, getters y setters de manera manual

--- backend/src/main/java/com/rogerloria/prueba_tecnica_backend/model/SolicitudCredito.java
+++ backend/src/main/java/com/rogerloria/prueba_tecnica_backend/model/SolicitudCredito.java
@@ -1,17 +1,56 @@
 package com.rogerloria.prueba_tecnica_backend.model;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
-@Data
 @Document(collection = "solicitudes")
 public class SolicitudCredito {
     @Id
     private String id;
-    private Usuario usuario;
+    private String usuarioId;
     private LocalDate fechaSolicitud;
-    private String estado; // "aprobada", "rechazada", "pendiente"
+    private String estado; // TODO: cambiar al tipo enum "aprobada", "rechazada", "pendiente"
+
+    public SolicitudCredito() {}
+
+    public SolicitudCredito(String id, String usuarioId, LocalDate fechaSolicitud, String estado) {
+        this.id = id;
+        this.usuarioId = usuarioId;
+        this.fechaSolicitud = fechaSolicitud;
+        this.estado = estado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuario(String usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public LocalDate getFechaSolicitud() {
+        return fechaSolicitud;
+    }
+
+    public void setFechaSolicitud(LocalDate fechaSolicitud) {
+        this.fechaSolicitud = fechaSolicitud;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
--- backend/src/main/java/com/rogerloria/prueba_tecnica_backend/model/Usuario.java
+++ backend/src/main/java/com/rogerloria/prueba_tecnica_backend/model/Usuario.java
@@ -1,11 +1,9 @@
 package com.rogerloria.prueba_tecnica_backend.model;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
-@Document(collection = "usuario")
+@Document(collection = "usuarios")
 public class Usuario {
     @Id
     private String id;
@@ -13,9 +11,103 @@ public class Usuario {
     private String apellidos;
     private String email;
     private String telefono;
-    private String tipoIdentificacion;
+    private String tipoIdentificacion; // TODO: cambiar el tipo de dato enum
     private String departamento;
     private String municipio;
     private String direccion;
     private Double ingresosMensuales;
+
+    public Usuario(String nombres, String apellidos, String email, String telefono, String tipoIdentificacion, String departamento, String municipio, String direccion, Double ingresosMensuales) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.tipoIdentificacion = tipoIdentificacion;
+        this.departamento = departamento;
+        this.municipio = municipio;
+        this.direccion = direccion;
+        this.ingresosMensuales = ingresosMensuales;
+    }
+
+    public Usuario() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipoIdentificacion() {
+        return tipoIdentificacion;
+    }
+
+    public void setTipoIdentificacion(String tipoIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Double getIngresosMensuales() {
+        return ingresosMensuales;
+    }
+
+    public void setIngresosMensuales(Double ingresosMensuales) {
+        this.ingresosMensuales = ingresosMensuales;
+    }
 }
